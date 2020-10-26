5e3dd18542f1c9fdd79876ab2a7a4a8df1a49c42
Michael Babich <olonore@gmail.com>
2020-10-26 21:48:37 +0200

    Rationals project added

---
+++ Rationals/Rational.cs
@@ -0,0 +1,77 @@
+﻿namespace Rationals
+{
+    public class Rational
+    {
+        private int _numerator;
+        private int _denominator;
+
+        public Rational(int numerator, int denominator)
+        {
+            if (denominator <= 0)
+                throw new InvalidValueException("Value Negative");
+
+            _numerator = numerator;
+            _denominator = denominator;
+        }
+
+        public bool GreaterThan(Rational other) 
+        {
+            return GetNumerator() * (long)other.GetDenominator()
+                > GetDenominator() * (long)other.GetNumerator();
+        }
+
+        public bool Equals(Rational other)
+        {
+            return GetNumerator() * (long)other.GetDenominator()
+                == GetDenominator() * (long)other.GetNumerator();
+        }
+
+        public static Rational operator +(Rational first, Rational second)
+        {
+            if (first.GetDenominator() * second.GetDenominator() <= 0)
+                throw new InvalidValueException("Value Overflow");
+
+            return new Rational(
+                (first.GetNumerator() * second.GetDenominator())
+                + (second.GetNumerator() * first.GetNumerator()),
+                first.GetDenominator() * second.GetDenominator());
+        }
+
+        public static Rational operator -(Rational first, Rational second)
+        {
+            if (first.GetDenominator() * second.GetDenominator() <= 0)
+                throw new InvalidValueException("Value Overflow");
+
+            return new Rational(
+                (first.GetNumerator() * second.GetDenominator())
+                - (second.GetNumerator() * first.GetNumerator()),
+                first.GetDenominator() * second.GetDenominator());
+        }
+
+        public static Rational operator *(Rational first, Rational second)
+        {
+            if (first.GetDenominator() * second.GetDenominator() <= 0)
+                throw new InvalidValueException("Value Overflow");
+
+            return new Rational(
+                first.GetNumerator() * second.GetNumerator(),
+                first.GetDenominator() * second.GetDenominator());
+        }
+
+        public int GetNumerator()
+        {
+            return _numerator;
+        }
+
+        public int GetDenominator()
+        {
+            return _denominator;
+        }
+
+        override
+        public string ToString()
+        {
+            return $"{GetNumerator()}/{GetDenominator()}";
+        }
+    }
+}
--- WordGuessingGame/Round.cs
+++ WordGuessingGame/Round.cs
@@ -4,7 +4,7 @@ using System.Linq;
 
 namespace WordGuessingGame
 {
-    public class Round : IGame
+    internal class Round : IGame
     {
         private int guesses = 0;
         private readonly List<char> word;
